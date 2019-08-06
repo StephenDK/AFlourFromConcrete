4bc4c47724e0c9e99ea75f90d815252983decaff
StephenDK <stephendklein08@gmail.com>
2019-08-06 11:20:26 -0700

    handlebars

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ const PORT = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("client/build"));
+app.use(express.static("public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/concreteflour";
 
