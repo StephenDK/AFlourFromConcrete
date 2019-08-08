267ad7901a41ad589ef13376257e6cb018b4c67d
StephenDK <stephendklein08@gmail.com>
2019-08-08 09:18:04 -0700

    Error handling

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,21 @@
 
 
 function submitData() {
-    var description = document.getElementById("getData")
+    var description = document.getElementById("getData");
+    if (description.value != '') {
+        $.ajax({
+            type: "POST",
+            url: "/api/post",
+            data: {
+                userText: description.value
+            }
+        })
+        
+        console.log("JSON posted");
+        description.value = '';
+    } else {
+        alert("You must enter something");
+    }
 
-    $.ajax({
-        type: "POST",
-        url: "/api/post",
-        data: {
-            userText: description.value
-        }
-    })
-
-    console.log("JSON posted");
-    description.value = "";
+    
 }
