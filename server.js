2c7810b9739f41c50577039f34a70b7ad6b73e25
StephenDK <stephendklein08@gmail.com>
2019-08-07 23:19:33 -0700

    Heroku test2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Keys = require("./config/keys");
+//const Keys = require("./config/keys");
 
 // Require db model
 require('./models/post-model');
